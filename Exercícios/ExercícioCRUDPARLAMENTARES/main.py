38498c1a0ac96e734cda1fa6d56b8e66ca56a407
Luiz Felipe <luizfelipenascimentogomes@outlook.com>
2024-03-31 03:41:31 -0300

    Fix: CRUD

--- Exercícios/ExercícioCRUDPARLAMENTARES/main.py
+++ Exercícios/ExercícioCRUDPARLAMENTARES/main.py
@@ -1,51 +1,79 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from datetime import datetime
 
 import json
 
 app = Flask('app')
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
-
 deputados = []
 frentes_parlamentares = []
 membros_frentes = []
 
 with open('dataset_deputados.json') as arquivo: 
-  data = arquivo.read()
-  data_dict = json.loads(data)
-  deputados = data_dict['dados']
+    data = arquivo.read()
+    data_dict = json.loads(data)
+    deputados = data_dict['dados']
 
 with open('dataset_frentes.json') as arquivo: 
-  data = arquivo.read()
-  data_dict = json.loads(data)
-  frentes_parlamentares = data_dict['dados']
+    data = arquivo.read()
+    data_dict = json.loads(data)
+    frentes_parlamentares = data_dict['dados']
 
 with open('all_frentes_membros.json') as arquivo: 
-  data = arquivo.read()
-  data_dict = json.loads(data)
-  membros_frentes = data_dict['dados']
+    data = arquivo.read()
+    data_dict = json.loads(data)
+    membros_frentes = data_dict['dados']
 
-@app.route('/dados/deputados')
-def dados_deputados():
-  return deputados
+@app.route('/deputados', methods=['GET'])
+def get_deputados():
+    return jsonify(deputados)
 
-@app.route('/dados/frentes')
-def dados_frentes_parlamentares():
-  return frentes_parlamentares
+@app.route('/deputados', methods=['POST'])
+def add_deputado():
+    novo_deputado = request.json
+    deputados.append(novo_deputado)
+    return jsonify({'message': 'Deputado adicionado com sucesso!'})
 
-@app.route('/dados/membros-frentes')
-def dados_membros_frentes():
-  return membros_frentes
+@app.route('/deputados/<int:id>', methods=['GET'])
+def get_deputado_by_id(id):
+    for deputado in deputados:
+        if deputado['id'] == id:
+            return jsonify(deputado)
+    return jsonify({'message': 'Deputado não encontrado'})
 
-@app.route('/deputado/nome/<string:nome>')
+@app.route('/deputados/<int:id>', methods=['PUT'])
+def update_deputado(id):
+    for i, deputado in enumerate(deputados):
+        if deputado['id'] == id:
+            deputados[i] = request.json
+            return jsonify({'message': 'Deputado atualizado com sucesso!'})
+    return jsonify({'message': 'Deputado não encontrado'})
+
+@app.route('/deputados/<int:id>', methods=['DELETE'])
+def delete_deputado(id):
+    for i, deputado in enumerate(deputados):
+        if deputado['id'] == id:
+            del deputados[i]
+            return jsonify({'message': 'Deputado removido com sucesso!'})
+    return jsonify({'message': 'Deputado não encontrado'})
+
+@app.route('/frentes', methods=['GET'])
+def get_frentes_parlamentares():
+    return jsonify(frentes_parlamentares)
+
+@app.route('/deputados/nome/<string:nome>', methods=['GET'])
 def get_deputado_by_nome(nome):
-  deputado = 'NÃO ENCONTRADO'
-  for item in deputados:
-    if nome in item['nome']:
-      deputado = item
-  return deputado
+    result = [dep for dep in deputados if nome.lower() in dep['nome'].lower()]
+    return jsonify(result)
 
-app.run(host='0.0.0.0', port=8080)
+
+@app.route('/frentes/<int:id>/deputados', methods=['GET'])
+def get_deputados_by_frente(id):
+    deputados_frente = []
+    for membro in membros_frentes:
+        if membro['frente_id'] == id:
+            deputados_frente.append(get_deputado_by_id(membro['deputado_id']))
+    return jsonify(deputados_frente)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
